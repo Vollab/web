c4a8c600ffe9fdcf5e33bc72ebc789322d36a10d
InSTinToS <miguelandradebarreto2@gmail.com>
2023-11-08 21:53:40 -0300

    :bug: ignores Typescript errors

--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
 /** @type {import('next-pwa').PWAConfig} */
 const withPWA = require('next-pwa')({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
+  disable: process.env.NODE_ENV === 'development'
 })
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-
-  images: {domains: []
-  }
+  ignoreBuildErrors: true,
+  images: { domains: [] }
 }
 
 module.exports = withPWA(nextConfig)
