478c8736535bf561de4439cf719e4dbf4d70fd2a
InSTinToS <miguelandradebarreto2@gmail.com>
2023-12-11 08:53:30 -0300

    :bug: deployment bug

--- src/api/requests/demands/vacancies/get/getDemandVacancies.ts
+++ src/api/requests/demands/vacancies/get/getDemandVacancies.ts
@@ -1,11 +1,8 @@
 import { interceptor } from 'src/api/core/interceptor'
 
-import {
-  Params,
-  Response
-} from 'types-vollab/dist/src/modules/vacancy/api/demands/[demand_id]/vacancies/GET'
+import { Response } from 'types-vollab/dist/src/modules/vacancy/api/demands/[demand_id]/vacancies/GET'
 
-type TGetDemand = ({ demand_id }: Params) => Promise<Response>
+type TGetDemand = ({ demand_id }: any) => Promise<Response>
 
 export const getDemandVacancies: TGetDemand = ({ demand_id }) =>
   interceptor({
--- src/api/requests/demands/vacancies/get/useDemandVacancies.ts
+++ src/api/requests/demands/vacancies/get/useDemandVacancies.ts
@@ -2,16 +2,13 @@ import { getDemandVacancies } from './getDemandVacancies'
 
 import { useQuery } from 'src/hooks/ReactQuery/useQuery'
 
-import {
-  Params,
-  Response
-} from 'types-vollab/dist/src/modules/vacancy/api/demands/[demand_id]/vacancies/GET'
+import { Response } from 'types-vollab/dist/src/modules/vacancy/api/demands/[demand_id]/vacancies/GET'
 
 interface IUseDemandVacanciesQuery {
   response: Response
 }
 
-export const useDemandVacancies = ({ demand_id }: Params) =>
+export const useDemandVacancies = ({ demand_id }: any) =>
   useQuery<IUseDemandVacanciesQuery>(
     ['demand/vacancies', demand_id],
     () => getDemandVacancies({ demand_id }),
